d09baf163c1d1d8a7cf76a19f1131fbc5e5d8101
nouhshaer5-arch <nouhshaer5@gmail.com>
2026-06-01 15:36:38 +0300

    Add projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import { ProjectsList } from "@/components/projects/projects-list";
+
+export default function ProjectsPage() {
+  return (
+    <main className="w-full">
+      <ProjectsList />
+    </main>
+  );
+}
