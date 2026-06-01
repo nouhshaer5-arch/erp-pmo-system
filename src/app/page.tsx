b0c18567a246b16e9e2e1e105e4593dff3a0425f
nouhshaer5-arch <nouhshaer5@gmail.com>
2026-06-01 15:36:15 +0300

    Add home page

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { ExecutiveDashboard } from "@/components/dashboard/executive-dashboard";
+
+export default function Home() {
+  return (
+    <main className="w-full">
+      <ExecutiveDashboard />
+    </main>
+  );
+}
